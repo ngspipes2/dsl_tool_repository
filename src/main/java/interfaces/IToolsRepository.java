66b2792ba2dbbf71847c9a63ebf172c38c0cf448
calmenelias <39606@alunos.isel.ipl.pt>
2018-05-14 17:22:35 +0100

    some names changed

--- src/main/java/interfaces/IToolsRepository.java
+++ src/main/java/interfaces/IToolsRepository.java
@@ -11,8 +11,8 @@ public interface IToolsRepository {
     Map<String, Object> getConfig() throws ToolRepositoryException;
 
     Collection<IToolDescriptor> getAll() throws ToolRepositoryException;
-    IToolDescriptor get(String id) throws ToolRepositoryException;
-    void update(IToolDescriptor entity) throws ToolRepositoryException;
-    void insert(IToolDescriptor entity) throws ToolRepositoryException;
-    void delete(String id) throws ToolRepositoryException;
+    IToolDescriptor get(String toolName) throws ToolRepositoryException;
+    void update(IToolDescriptor tool) throws ToolRepositoryException;
+    void insert(IToolDescriptor tool) throws ToolRepositoryException;
+    void delete(String toolName) throws ToolRepositoryException;
 }
